23f3abc2b519366a554b0ddab635424557fad576
WilsonCabrera1996 <cabrerawilson22@gmail.com>
2024-06-13 02:05:38 -0500

    Actualizacion

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+export const ProductContext = createContext();
+
+const ProductProvider = ({ Agroforestal }) => {
+  
+  const [products, setProducts] = useState([]);
+  useEffect(()=>{
+    fetch('/data/productos.json')
+    .then((response) => response.json())
+    .then((data) => setProducts(data))
+    .catch((error) => console.error('Error al cargar los productos:', error));
+  }, [])
+  return <ProductContext.Provider value={{products}}>{Agroforestal}</ProductContext.Provider>;
 };
 
-export default ProductContext;
+export default ProductProvider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import { ProductContext } from '../contexts/ProductContext';
 
 const Home = () => {
+  const {products} = useContext(ProductContext);
+  console.log(products);
+  const filteredProducts = products.filter(item => {
+    return (item.categoria === "agroforestal");
+  })
+console.log (filteredProducts);
   return <div>Homepage</div>;
 };
 
