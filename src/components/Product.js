81c28e6c6dc8ffc666f16157365d5f0b7ac4eea6
WilsonCabrera1996 <cabrerawilson22@gmail.com>
2024-06-17 13:32:49 -0500

    Add Logo

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { BsEyeFill, BsWhatsapp } from 'react-icons/bs';
 
+
 const Product = ({ product }) => {
-  console.log(product);
+
+
   const { id, imagen, nombre, categoria, codigo } = product;
 
   const phoneNumber = '+593980433551';
